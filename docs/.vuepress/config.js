e8ca00ebaca2bfbf7ceaa12bb62b6418ba4e0d69
binghe_001 <1028386804@qq.com>
2022-05-08 15:54:28 +0800

    冰河：新增《SpringCloud Alibaba实战》专栏第12章内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -836,6 +836,7 @@ function getBarMicroServices() {
                 "springcloudalibaba/2022-04-28-SA实战-第9章-服务容错-服务雪崩与容错方案.md",
                 "springcloudalibaba/2022-05-03-SA实战-第10章-服务容错-Fegin整合Sentinel.md",
                 "springcloudalibaba/2022-05-05-SA实战-第11章-服务容错加餐-Sentinel核心技术与配置规则.md",
+                "springcloudalibaba/2022-05-08-SA实战-第12章-服务网关-网关概述与核心架构.md",
             ]
         }
     ]
