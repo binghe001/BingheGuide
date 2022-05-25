6b4bf6ebeea2b5f81418fd5a9353165476b7a0fd
binghe001 <1028386804@qq.com>
2022-05-25 11:41:45 +0800

    冰河：新增《SpringCloud Alibaba实战》第24章文章

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -853,6 +853,7 @@ function getBarMicroServices() {
                 "springcloudalibaba/2022-05-21-SA实战-第21章-服务配置-服务配置介绍与Nacos核心概念.md",
                 "springcloudalibaba/2022-05-23-SA实战-第22章-服务配置-项目整合Nacos配置中心.md",
                 "springcloudalibaba/2022-05-24-SA实战-第23章-服务配置-实现动态刷新与配置共享.md",
+                "springcloudalibaba/2022-05-25-SA实战-第24章-分布式事务-分布式事务核心原理与Seata介绍.md",
             ]
         }
     ]
