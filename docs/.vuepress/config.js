62552456848cfda6f2b22cb14d3c4ee1a10bb9be
binghe001 <1028386804@qq.com>
2023-03-15 21:13:38 +0800

    新增ChatGPT链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -570,6 +570,10 @@ module.exports = {
                     {
                         text: 'Github',
                         link: 'https://github.com/binghe001/BingheGuide'
+                    },
+                    {
+                        text: 'ChatGPT',
+                        link: 'https://chat.openai.run'
                     }
                 ],
                 sidebar: {
